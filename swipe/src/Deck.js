f24eb247a6b2c487554fe8759165e624dec6a807
Rex Low <qiweilow950823@gmail.com>
2017-07-03 21:05:24 +0800

    set up boilerplate for PanResponder

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react';
 
 import {
   Animated,
+  PanResponder,
   View,
 } from 'react-native';
 
 export default class Deck extends Component {
 
+  constructor(props) {
+    super(props);
+
+    // its a self-contained object
+    const panResponder = PanResponder.create({
+
+      
+      onStartShouldSetPanResponder: () => true, // gets called when user press on the screen
+      onPanResponderMove: (event, gesture) => { // callback when user move around the screen, dragging
+        
+      },
+      onPanResponderRelease: () => {}
+    });
+    
+    // this.panResponder = panResponder;
+
+    this.state = { panResponder }
+  }
+
   renderCard = () => this.props.data.map(item => this.props.renderCard(item))
 
   render() {
     return (
-      <View>
+      // passing callbacks to view component
+      <View {...this.state.panResponder.panHandlers}>
         {this.renderCard()}
       </View>
     )
