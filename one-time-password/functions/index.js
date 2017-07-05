b47d301de85be5c231dd126779f3d6a8e4b784a4
Rex Low <qiweilow950823@gmail.com>
2017-07-05 13:20:09 +0800

    added service account, gitignore

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,8 +1,6 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const createUser = require('./create_user');
+
+exports.createUser = functions.https.onRequest(createUser);
