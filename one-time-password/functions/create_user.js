2cb448a22b51a3abb25c426db6153172a858b558
Rex Low <qiweilow950823@gmail.com>
2017-07-06 12:34:46 +0800

    created createUser API

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,3 +1,18 @@
+const admin = require('firebase-admin');
+
 module.exports = (req, res) => {
-  res.send(req.body);
+  // verify that user has provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input!'});
+  }
+
+  // format the phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, ""); // replace anything that id not a digit with empty string
+
+  // create a new user account using the phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(error => res.status(422).send({ error: error }));
+
+  // response to user request, saying account is created
 }
