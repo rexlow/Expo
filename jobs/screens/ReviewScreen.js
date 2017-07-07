2a14cd23f8815799dc4d28a2d023ed30e3f15176
Rex Low <qiweilow950823@gmail.com>
2017-07-07 15:33:16 +0800

    add margin for android

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 import Reactotron from 'reactotron-react-native';
 import { Button } from 'react-native-elements';
@@ -7,18 +7,19 @@ import { Button } from 'react-native-elements';
 
 export default class ReviewScreen extends Component {
 
-  // static navigationOptions = {
-  //   title: "Review Jobs",
-  //   header: ({ navigation }) => {
-  //     return {
-  //       headerRight: <Button title="Settings" onPress={() => navigation.navigate('settings')}/>
-  //     };
-  //   }
-  // }
-
   static navigationOptions = ({ navigation }) => ({
     title: "Review Jobs",
-    headerRight: <Button title="Settings" onPress={() => navigation.navigate('settings')} />
+    headerRight: (
+      <Button 
+        title="Settings" 
+        onPress={() => navigation.navigate('settings')}
+        backgroundColor="rgba(0,0,0,0)"
+        color="rgba(0, 122, 255, 1)"
+      />
+    ),
+    headerStyle: {
+      marginTop: (Platform.OS === 'android' ? 24 : 0)
+    }
   })
 
   render() {
