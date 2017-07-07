dfc97eec34cc45ba37e2b6cbc3ec77b0e21699fd
Rex Low <qiweilow950823@gmail.com>
2017-07-07 14:10:19 +0800

    installed react-navigation

--- jobs/App.js
+++ jobs/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Reactotron from 'reactotron-react-native';
 import './ReactotronConfig'
 
 export default class App extends React.Component {
   render() {
+    Reactotron.log('yoyo')
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
