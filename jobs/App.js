b720f11cdf435fff48c21afc6cb75196a2a7dad7
Rex Low <qiweilow950823@gmail.com>
2017-07-07 13:23:32 +0800

    installed reactotron

--- jobs/App.js
+++ jobs/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import './ReactotronConfig'
+
 export default class App extends React.Component {
   render() {
     return (
