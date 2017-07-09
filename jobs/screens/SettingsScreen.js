596699fe9a83ff407ca9a461e6ee6547dbef8809
Rex Low <qiweilow950823@gmail.com>
2017-07-09 21:59:36 +0800

    missing a Platform keyword

--- jobs/screens/SettingsScreen.js
+++ jobs/screens/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 import { connect } from 'react-redux';
 import { clearLikedJobs } from '../actions';
